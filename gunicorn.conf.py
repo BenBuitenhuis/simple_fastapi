1b8c18a4361e18e041927636e7b269d299eebddf
mrnobody <jesavius@yahoo.com>
2020-12-31 17:42:44 +0000

    📦 NEW: Lowering to two workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,6 @@
 Gunicorn with Uvicorn config to launch in Digital Ocean's App Platform.
 """
 bind = "0.0.0.0:8080"
-workers = 4
+workers = 2
 # Uvicorn's Gunicorn worker class
 worker_class = "uvicorn.workers.UvicornWorker"
